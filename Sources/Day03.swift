957aef6e150a7f33029f09216b1ef21e7f7f1ecc
yutailang0119 <yutailang0119@gmail.com>
2024-12-03 16:01:58 +0900

    Fix String to add muls

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -1,4 +1,5 @@
 import Foundation
+import RegexBuilder
 
 struct Day03: AdventDay {
   var data: String
@@ -12,5 +13,31 @@ private extension Day03 {
   struct Mul {
     var left: Int
     var right: Int
+  }
+}
+
+private extension String {
+  var muls: [Day03.Mul] {
+    let regex = Regex {
+      One("mul")
+      One("(")
+      Capture {
+        ZeroOrMore(.digit)
+      }
+      One(",")
+      Capture {
+        ZeroOrMore(.digit)
+      }
+      One(")")
+    }
+
+    return matches(of: regex).compactMap { matched in
+      guard let left = Int(matched.output.1),
+        let right = Int(matched.output.2)
+      else {
+        return nil
+      }
+      return Day03.Mul(left: left, right: right)
+    }
   }
 }
