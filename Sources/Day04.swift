91b20a1ad97afe3e931e2a391dc8cc882308367d
yutailang0119 <yutailang0119@gmail.com>
2024-12-04 17:59:53 +0900

    Fux Day04 to add entities

--- Sources/Day04.swift
+++ Sources/Day04.swift
@@ -3,6 +3,11 @@ import Foundation
 struct Day04: AdventDay {
   var data: String
 
+  var entities: [[String]] {
+    data.split(separator: "\n")
+      .map { $0.map(String.init) }
+  }
+
   func part1() async throws -> Any {
     0
   }
