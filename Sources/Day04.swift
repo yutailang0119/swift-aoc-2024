25b13684be4f09855e56dbdffd8b799e43c3ba1f
yutailang0119 <yutailang0119@gmail.com>
2024-12-04 18:01:55 +0900

    Fix Day04 to add Word, Cursor

--- Sources/Day04.swift
+++ Sources/Day04.swift
@@ -12,3 +12,26 @@ struct Day04: AdventDay {
     0
   }
 }
+
+private extension Day04 {
+  enum Word: String {
+    case x = "X"
+    case m = "M"
+    case a = "A"
+    case s = "S"
+
+    var next: Word? {
+      switch self {
+      case .x: return .m
+      case .m: return .a
+      case .a: return .s
+      case .s: return nil
+      }
+    }
+  }
+
+  struct Cursor {
+    var word: Word
+    var position: Puzzle.Position
+  }
+}
