41e5b94f70d7cd15e4280ab2f2bdd02eb026cb1b
yutailang0119 <yutailang0119@gmail.com>
2024-12-05 18:04:28 +0900

    Fix Day05 to add entities

--- Sources/Day05.swift
+++ Sources/Day05.swift
@@ -3,6 +3,11 @@ import Foundation
 struct Day05: AdventDay {
   var data: String
 
+  var entities: [String] {
+    data.split(separator: "\n\n")
+      .map(String.init)
+  }
+
   func part1() async throws -> Any {
     0
   }
