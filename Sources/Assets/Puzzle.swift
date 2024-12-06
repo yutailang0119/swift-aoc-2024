20da026920ec31ea00171f316ed685ee25cd156c
yutailang0119 <yutailang0119@gmail.com>
2024-12-07 00:16:26 +0900

    Fix Puzzle to cleanup

--- Sources/Assets/Puzzle.swift
+++ Sources/Assets/Puzzle.swift
@@ -3,50 +3,11 @@ enum Puzzle {}
 extension Puzzle {
   struct Table<Element: Equatable> {
     var lines: [[Element]]
-
-    func element(at position: Position) -> Element? {
-      lines[safe: position.y]?[safe: position.x]
-    }
-
-    func positions(for element: Element) -> [Position] {
-      var positions: [Position] = []
-      for line in lines.enumerated() {
-        for e in line.element.enumerated() {
-          if e.element == element {
-            positions.append(Position(x: e.offset, y: line.offset))
-          }
-        }
-      }
-      return positions
-    }
   }
 
   struct Position: Hashable {
     var x: Int
     var y: Int
-
-    func moved(to direction: Direction) -> Position {
-      switch direction {
-      case .top:
-        return Position(x: x, y: y - 1)
-      case .bottom:
-        return Position(x: x, y: y + 1)
-      case .left:
-        return Position(x: x - 1, y: y)
-      case .right:
-        return Position(x: x + 1, y: y)
-      case [.top, .left]:
-        return Position(x: x - 1, y: y - 1)
-      case [.top, .right]:
-        return Position(x: x + 1, y: y - 1)
-      case [.bottom, .left]:
-        return Position(x: x - 1, y: y + 1)
-      case [.bottom, .right]:
-        return Position(x: x + 1, y: y + 1)
-      default:
-        fatalError()
-      }
-    }
   }
 
   struct Direction: OptionSet {
@@ -56,5 +17,48 @@ extension Puzzle {
     static let bottom = Direction(rawValue: 1 << 1)
     static let left = Direction(rawValue: 1 << 2)
     static let right = Direction(rawValue: 1 << 3)
+  }
+}
+
+extension Puzzle.Table {
+  func element(at position: Puzzle.Position) -> Element? {
+    lines[safe: position.y]?[safe: position.x]
+  }
+
+  func positions(for element: Element) -> [Puzzle.Position] {
+    var positions: [Puzzle.Position] = []
+    for line in lines.enumerated() {
+      for e in line.element.enumerated() {
+        if e.element == element {
+          positions.append(Puzzle.Position(x: e.offset, y: line.offset))
+        }
+      }
+    }
+    return positions
+  }
+}
+
+extension Puzzle.Position {
+  func moved(to direction: Puzzle.Direction) -> Self {
+    switch direction {
+    case .top:
+      return Self(x: x, y: y - 1)
+    case .bottom:
+      return Self(x: x, y: y + 1)
+    case .left:
+      return Self(x: x - 1, y: y)
+    case .right:
+      return Self(x: x + 1, y: y)
+    case [.top, .left]:
+      return Self(x: x - 1, y: y - 1)
+    case [.top, .right]:
+      return Self(x: x + 1, y: y - 1)
+    case [.bottom, .left]:
+      return Self(x: x - 1, y: y + 1)
+    case [.bottom, .right]:
+      return Self(x: x + 1, y: y + 1)
+    default:
+      fatalError()
+    }
   }
 }
