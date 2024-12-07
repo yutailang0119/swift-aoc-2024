0ac250db25ae41510c819b63350c0c5b72ceb8cd
yutailang0119 <yutailang0119@gmail.com>
2024-12-07 16:59:31 +0900

    Fix Day06.part2 to use withTaskGroup(of:returning:isolation:body:)

--- Sources/Day06.swift
+++ Sources/Day06.swift
@@ -25,34 +25,46 @@ struct Day06: AdventDay {
     }
     route.remove(`guard`)
 
-    var loopedCount = 0
-    for position in route {
-      var t = table
-      t.lines[position.y][position.x] = .obstruction
+    let looped = await withTaskGroup(of: Bool.self) { group in
+      for position in route {
+        group.addTask {
+          var t = table
+          t.lines[position.y][position.x] = .obstruction
 
-      var routes: [[Puzzle.Position]] = []
-      var cursor: Puzzle.Position? = `guard`
-      var direction: Puzzle.Direction = .top
-      while let c = cursor {
-        let route = t.route(from: c, to: direction, until: .obstruction)
-        if routes.contains(route) {
-          loopedCount += 1
-          cursor = nil
-        } else {
-          if !route.isEmpty {
-            routes.append(route)
+          var routes: [[Puzzle.Position]] = []
+          var cursor: Puzzle.Position? = `guard`
+          var direction: Puzzle.Direction = .top
+          while let c = cursor {
+            let route = t.route(from: c, to: direction, until: .obstruction)
+            if routes.contains(route) {
+              cursor = nil
+              return true
+            } else {
+              if !route.isEmpty {
+                routes.append(route)
+              }
+              if let last = routes.flatMap({ $0 }).last,
+                 t.element(at: last.moved(to: direction)) == .obstruction {
+                cursor = last
+                direction = direction.rightDegrees
+              } else {
+                cursor = nil
+                return false
+              }
+            }
           }
-          if let last = routes.flatMap({ $0 }).last,
-             t.element(at: last.moved(to: direction)) == .obstruction {
-            cursor = last
-            direction = direction.rightDegrees
-          } else {
-            cursor = nil
-          }
+          fatalError()
         }
       }
+
+      var looped: [Bool] = []
+      for await g in group {
+        looped.append(g)
+      }
+      return looped
     }
-    return loopedCount
+
+    return looped.count { $0 }
   }
 
   private func route(in table: Puzzle.Table<Grid>) -> [Puzzle.Position] {
@@ -81,7 +93,7 @@ struct Day06: AdventDay {
 }
 
 private extension Day06 {
-  enum Grid: String, CustomStringConvertible {
+  enum Grid: String, Sendable, CustomStringConvertible {
     case `guard` = "^"
     case obstruction = "#"
     case dot = "."
