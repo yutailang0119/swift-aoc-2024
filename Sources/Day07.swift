43acf2de37499eec69ca6e8a4c3deeb50fd1d408
yutailang0119 <yutailang0119@gmail.com>
2024-12-07 19:23:14 +0900

    Fix Day07 to add equations

--- Sources/Day07.swift
+++ Sources/Day07.swift
@@ -11,6 +11,17 @@ struct Day07: AdventDay {
   func part1() async throws -> Any {
     0
   }
+
+  private var equations: [Equation] {
+    entities.compactMap {
+      let splited =  $0.split(separator: ": ")
+      guard let test = Int(splited[0]) else {
+        return nil
+      }
+      let numbers = splited[1].split(separator: " ").compactMap { Int($0) }
+      return Equation(test: test, numbers: numbers)
+    }
+  }
 }
 
 private extension Day07 {
