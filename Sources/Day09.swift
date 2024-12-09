aee9a5cd932710c53b51bb1f02a1bc58239b1884
yutailang0119 <yutailang0119@gmail.com>
2024-12-09 19:32:28 +0900

    Fix Day09 to add DiskMap, Block

--- Sources/Day09.swift
+++ Sources/Day09.swift
@@ -11,3 +11,47 @@ struct Day09: AdventDay {
     0
   }
 }
+
+private extension Day09 {
+  struct DiskMap: CustomStringConvertible {
+    var blocks: [Block]
+
+    var checksum: Int {
+      var checksum = 0
+      for (offset, block) in blocks.enumerated() {
+        checksum += offset * block.num
+      }
+      return checksum
+    }
+
+    var description: String {
+      blocks.map(\.description).joined()
+    }
+  }
+
+  enum Block: CustomStringConvertible {
+    case number(Int)
+    case space
+
+    var isSpace: Bool {
+      switch self {
+      case .number: return false
+      case .space: return true
+      }
+    }
+
+    var num: Int {
+      switch self {
+      case .number(let num): return num
+      case .space: return 0
+      }
+    }
+
+    var description: String {
+      switch self {
+      case .number(let n): return "\(n)"
+      case .space: return "."
+      }
+    }
+  }
+}
