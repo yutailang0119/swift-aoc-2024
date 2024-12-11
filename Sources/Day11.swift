4fe941c9e94c25e565844669c7f758d6d6c48882
yutailang0119 <yutailang0119@gmail.com>
2024-12-11 18:45:53 +0900

    Fix Day11 to add Int.divided

--- Sources/Day11.swift
+++ Sources/Day11.swift
@@ -15,3 +15,21 @@ private extension Day11 {
     }
   }
 }
+
+private extension Int {
+  var divided: (leading: Int, trailing: Int?) {
+    guard self != 0 else {
+      return (1, nil)
+    }
+    let text = Array("\(self)")
+    let digits = text.count
+    if digits.isMultiple(of: 2) {
+      let center = digits / 2
+      let leading = text.prefix(center)
+      let trailing = text.suffix(center)
+      return (Int(String(leading))!, Int(String(trailing)))
+    } else {
+      return (self * 2024, nil)
+    }
+  }
+}
