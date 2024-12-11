15dd8328c44cdc86b59870cb7be434bd9852288a
yutailang0119 <yutailang0119@gmail.com>
2024-12-11 22:15:45 +0900

    Fix Day11 to add count(for:to:)

--- Sources/Day11.swift
+++ Sources/Day11.swift
@@ -39,6 +39,34 @@ private extension Day11 {
     }
     return stns
   }
+
+  func count(for stones: [Int], to count: Int) -> Int {
+    func rearrangement(_ stns: [Int: Int]) -> [Int: Int] {
+      var outputs = [Int: Int]()
+      for stn in stns {
+        if stn.key == 0 {
+          outputs[1, default: 0] += stn.value
+          continue
+        }
+        let text = "\(stn.key)"
+        let digits = text.count
+        if digits.isMultiple(of: 2) {
+          let center = digits / 2
+          outputs[Int(text.prefix(center))!, default: 0] += stn.value
+          outputs[Int(text.suffix(center))!, default: 0] += stn.value
+        } else {
+          outputs[stn.key * 2024, default: 0] += stn.value
+        }
+      }
+      return outputs
+    }
+
+    var dictionary: [Int: Int] = Dictionary(grouping: stones) { $0 }.mapValues(\.count)
+    for _ in 0..<count {
+      dictionary = rearrangement(dictionary)
+    }
+    return dictionary.values.reduce(0, +)
+  }
 }
 
 private extension Int {
