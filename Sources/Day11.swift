972e91cb6db68e8d3c90c6aec2e67d0075692e96
yutailang0119 <yutailang0119@gmail.com>
2024-12-11 22:20:30 +0900

    Fix Day11 to remove rearrangement(for:to:)

--- Sources/Day11.swift
+++ Sources/Day11.swift
@@ -16,30 +16,6 @@ private extension Day11 {
     }
   }
 
-  func rearrangement(for stones: [Int], to count: Int) -> [Int] {
-    var dictionary: [Int: Divided] = [:]
-    var stns = stones
-    for i in 0..<count {
-      let s = stns.reduce(into: [Int]()) {
-        if let divided = dictionary[$1] {
-          $0.append(divided.leading)
-          if let trailing = divided.trailing {
-            $0.append(trailing)
-          }
-        } else {
-          let divided = $1.divided
-          $0.append(divided.leading)
-          if let trailing = divided.trailing {
-            $0.append(trailing)
-          }
-          dictionary[$1] = divided
-        }
-      }
-      stns = s
-    }
-    return stns
-  }
-
   func count(for stones: [Int], to count: Int) -> Int {
     func rearrangement(_ stns: [Int: Int]) -> [Int: Int] {
       var outputs = [Int: Int]()
