8c9d475ac9b5a3f51af8d0246d389a192b2e5b5b
yutailang0119 <yutailang0119@gmail.com>
2024-12-11 19:35:27 +0900

    Fix Day11 to update performance

--- Sources/Day11.swift
+++ Sources/Day11.swift
@@ -17,13 +17,22 @@ private extension Day11 {
   }
 
   func rearrangement(for stones: [Int], to count: Int) -> [Int] {
+    var dictionary: [Int: Divided] = [:]
     var stns = stones
-    for _ in 0..<count {
+    for i in 0..<count {
       let s = stns.reduce(into: [Int]()) {
-        let divided = $1.divided
-        $0.append(divided.leading)
-        if let trailing = divided.trailing {
-          $0.append(trailing)
+        if let divided = dictionary[$1] {
+          $0.append(divided.leading)
+          if let trailing = divided.trailing {
+            $0.append(trailing)
+          }
+        } else {
+          let divided = $1.divided
+          $0.append(divided.leading)
+          if let trailing = divided.trailing {
+            $0.append(trailing)
+          }
+          dictionary[$1] = divided
         }
       }
       stns = s
