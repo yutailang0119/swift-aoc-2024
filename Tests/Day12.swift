8b64d0e4903eb2663b5fbe23a6dd93e9703fede1
yutailang0119 <yutailang0119@gmail.com>
2024-12-13 01:18:28 +0900

    Fix Day12Tests to add testPart2

--- Tests/Day12.swift
+++ Tests/Day12.swift
@@ -45,4 +45,72 @@ struct Day12Tests {
       try await #expect(String(describing: challenge.part1()) == "1930")
     }
   }
+
+  @Test func testPart2() async throws {
+    do {
+      let testData = """
+        AAAA
+        BBCD
+        BBCC
+        EEEC
+
+        """
+      let challenge = Day12(data: testData)
+      try await #expect(String(describing: challenge.part2()) == "80")
+    }
+    do {
+      let testData = """
+        OOOOO
+        OXOXO
+        OOOOO
+        OXOXO
+        OOOOO
+
+        """
+      let challenge = Day12(data: testData)
+      try await #expect(String(describing: challenge.part2()) == "436")
+    }
+    do {
+      let testData = """
+        EEEEE
+        EXXXX
+        EEEEE
+        EXXXX
+        EEEEE
+
+        """
+      let challenge = Day12(data: testData)
+      try await #expect(String(describing: challenge.part2()) == "236")
+    }
+    do {
+      let testData = """
+        AAAAAA
+        AAABBA
+        AAABBA
+        ABBAAA
+        ABBAAA
+        AAAAAA
+
+        """
+      let challenge = Day12(data: testData)
+      try await #expect(String(describing: challenge.part2()) == "368")
+    }
+    do {
+      let testData = """
+        RRRRIICCFF
+        RRRRIICCCF
+        VVRRRCCFFF
+        VVRCCCJFFF
+        VVVVCJJCFE
+        VVIVCCJJEE
+        VVIIICJJEE
+        MIIIIIJJEE
+        MIIISIJEEE
+        MMMISSJEEE
+
+        """
+      let challenge = Day12(data: testData)
+      try await #expect(String(describing: challenge.part2()) == "1206")
+    }
+  }
 }
