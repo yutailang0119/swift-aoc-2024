9cfbe457098884ecde93f05fa3cb52904b13ab2a
yutailang0119 <yutailang0119@gmail.com>
2024-12-12 16:32:33 +0900

    Add Day12 assets

---
+++ Sources/Day12.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct Day12: AdventDay {
+  var data: String
+
+  func part1() async throws -> Any {
+    0
+  }
+}
---
+++ Tests/Day12.swift
@@ -0,0 +1,49 @@
+import Testing
+
+@testable import AdventOfCode
+
+struct Day12Tests {
+  let testData = """
+    RRRRIICCFF
+    RRRRIICCCF
+    VVRRRCCFFF
+    VVRCCCJFFF
+    VVVVCJJCFE
+    VVIVCCJJEE
+    VVIIICJJEE
+    MIIIIIJJEE
+    MIIISIJEEE
+    MMMISSJEEE
+
+    """
+
+  @Test func testPart1() async throws {
+    do {
+      let testData = """
+        AAAA
+        BBCD
+        BBCC
+        EEEC
+
+        """
+      let challenge = Day12(data: testData)
+      try await #expect(String(describing: challenge.part1()) == "140")
+    }
+    do {
+      let testData = """
+        OOOOO
+        OXOXO
+        OOOOO
+        OXOXO
+        OOOOO
+
+        """
+      let challenge = Day12(data: testData)
+      try await #expect(String(describing: challenge.part1()) == "772")
+    }
+    do {
+      let challenge = Day12(data: testData)
+      try await #expect(String(describing: challenge.part1()) == "1930")
+    }
+  }
+}
