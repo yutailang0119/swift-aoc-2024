0fc37ba93ac0a8fdf337758c34dda98a963f7205
yutailang0119 <yutailang0119@gmail.com>
2024-12-13 16:45:51 +0900

    Fix Day13 to add String.machine with RegexBuilder

--- Sources/Day13.swift
+++ Sources/Day13.swift
@@ -1,4 +1,5 @@
 import Foundation
+import RegexBuilder
 
 struct Day13: AdventDay {
   var data: String
@@ -25,5 +26,58 @@ private extension Day13 {
     var buttonA: Claw
     var buttonB: Claw
     var prize: Claw
+  }
+}
+
+private extension String {
+  var machine: Day13.Machine? {
+    let regex = Regex {
+      Capture {
+        ChoiceOf {
+          One("Button A")
+          One("Button B")
+          One("Prize")
+        }
+      }
+      One(": ")
+      Capture {
+        ChoiceOf {
+          One("X+")
+          One("X=")
+        }
+        Capture {
+          OneOrMore(.digit)
+        }
+      }
+      One(", ")
+      Capture {
+        ChoiceOf {
+          One("Y+")
+          One("Y=")
+        }
+        Capture {
+          OneOrMore(.digit)
+        }
+      }
+    }
+
+    var dictionary: [String: (x: Int, y: Int)] = [:]
+    for match in matches(of: regex) {
+      let output = match.output
+      dictionary[String(output.1)] = (x: Int(output.3)!, y: Int(output.5)!)
+    }
+
+    guard let buttonA = dictionary["Button A"],
+      let buttonB = dictionary["Button B"],
+      let prize = dictionary["Prize"]
+    else {
+      return nil
+    }
+
+    return Day13.Machine(
+      buttonA: Day13.Machine.Claw(x: buttonA.x, y: buttonA.y),
+      buttonB: Day13.Machine.Claw(x: buttonB.x, y: buttonB.y),
+      prize: Day13.Machine.Claw(x: prize.x, y: prize.y)
+    )
   }
 }
