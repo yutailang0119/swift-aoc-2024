469e5c0b795b5d07d7d24636e06b041b19a9d2fa
yutailang0119 <yutailang0119@gmail.com>
2024-12-14 14:29:20 +0900

    Add DAy14 assets

---
+++ Tests/Day14.swift
@@ -0,0 +1,26 @@
+import Testing
+
+@testable import AdventOfCode
+
+struct Day14Tests {
+  let testData = """
+    p=0,4 v=3,-3
+    p=6,3 v=-1,-3
+    p=10,3 v=-1,2
+    p=2,0 v=2,-1
+    p=0,0 v=1,3
+    p=3,0 v=-2,-2
+    p=7,6 v=-1,-3
+    p=3,0 v=-1,-2
+    p=9,3 v=2,3
+    p=7,3 v=-1,2
+    p=2,4 v=2,-3
+    p=9,5 v=-3,-3
+
+    """
+
+  @Test func testPart1() async throws {
+    let challenge = Day14(data: testData)
+    try await #expect(String(describing: challenge.part1()) == "12")
+  }
+}
