40bafa192d0c23636e8600ae26f8398681afdfd7
yutailang0119 <yutailang0119@gmail.com>
2024-12-15 22:50:44 +0900

    Add Day15 assets

---
+++ Sources/Day15.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct Day15: AdventDay {
+  var data: String
+
+  func part1() async throws -> Any {
+    0
+  }
+}
---
+++ Tests/Day15.swift
@@ -0,0 +1,54 @@
+import Testing
+
+@testable import AdventOfCode
+
+struct Day15Tests {
+  let testData = """
+    ##########
+    #..O..O.O#
+    #......O.#
+    #.OO..O.O#
+    #..O@..O.#
+    #O#..O...#
+    #O..O..O.#
+    #.OO.O.OO#
+    #....O...#
+    ##########
+
+    <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+    vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+    ><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+    <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+    ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+    ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+    >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+    <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+    ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+    v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+
+    """
+
+  @Test func testPart1() async throws {
+    do {
+      let testData = """
+        ########
+        #..O.O.#
+        ##@.O..#
+        #...O..#
+        #.#.O..#
+        #...O..#
+        #......#
+        ########
+
+        <^^>>>vv<v>>v<<
+
+        """
+      let challenge = Day15(data: testData)
+      try await #expect(String(describing: challenge.part1()) == "2028")
+    }
+    do {
+      let challenge = Day15(data: testData)
+      try await #expect(String(describing: challenge.part1()) == "10092")
+    }
+  }
+}
