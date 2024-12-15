a7ee021a00a8a5a5fbb366df83a721464a962a72
yutailang0119 <yutailang0119@gmail.com>
2024-12-16 01:46:54 +0900

    Fix Day15 to add Grid, Mode

--- Sources/Day15.swift
+++ Sources/Day15.swift
@@ -13,3 +13,19 @@ private extension Day15 {
     data.split(separator: "\n\n").map(String.init)
   }
 }
+
+private extension Day15 {
+  enum Grid: Character {
+    case robot = "@"
+    case box = "O"
+    case wall = "#"
+    case empty = "."
+  }
+
+  enum Move: Character {
+    case up = "^"
+    case down = "v"
+    case left = "<"
+    case right = ">"
+  }
+}
