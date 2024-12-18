8659b73cf8934ef50875ff27bb49e6d90ca511f9
yutailang0119 <yutailang0119@gmail.com>
2024-12-19 00:31:43 +0900

    Fix Day17Tests to cleanup

--- Tests/AdventOfCodeTests/Day17.swift
+++ Tests/AdventOfCodeTests/Day17.swift
@@ -3,15 +3,14 @@ import Testing
 @testable import AdventOfCode
 
 struct Day17Tests {
-  let testData = """
-    Register A: 729
-    Register B: 0
-    Register C: 0
-
-    Program: 0,1,5,4,3,0
-    """
-
   @Test func testPart1() async throws {
+    let testData = """
+      Register A: 729
+      Register B: 0
+      Register C: 0
+
+      Program: 0,1,5,4,3,0
+      """
     let challenge = Day17(data: testData)
     try await #expect(String(describing: challenge.part1()) == "4,6,3,5,6,3,5,2,1,0")
   }
