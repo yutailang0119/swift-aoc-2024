de9201bdfd79f0c56d78d18280f934bd3dc9dc2f
yutailang0119 <yutailang0119@gmail.com>
2024-12-19 00:32:00 +0900

    Fix Day17Tests to add testPart2

--- Tests/AdventOfCodeTests/Day17.swift
+++ Tests/AdventOfCodeTests/Day17.swift
@@ -14,4 +14,16 @@ struct Day17Tests {
     let challenge = Day17(data: testData)
     try await #expect(String(describing: challenge.part1()) == "4,6,3,5,6,3,5,2,1,0")
   }
+
+  @Test func testPart2() async throws {
+    let testData = """
+      Register A: 2024
+      Register B: 0
+      Register C: 0
+
+      Program: 0,3,5,4,3,0
+      """
+    let challenge = Day17(data: testData)
+    try await #expect(String(describing: challenge.part2()) == "117440")
+  }
 }
