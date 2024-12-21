29b7691eea1dab8dc36670bc7bba920bb851e33e
yutailang0119 <yutailang0119@gmail.com>
2024-12-22 05:19:47 +0900

    Add Day21 assets

---
+++ Tests/AdventOfCodeTests/Day21.swift
@@ -0,0 +1,19 @@
+import Testing
+
+@testable import AdventOfCode
+
+struct Day21Tests {
+  let testData = """
+    029A
+    980A
+    179A
+    456A
+    379A
+
+    """
+
+  @Test func testPart1() async throws {
+    let challenge = Day21(data: testData)
+    try await #expect(String(describing: challenge.part1()) == "126384")
+  }
+}
