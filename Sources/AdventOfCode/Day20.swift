9a4c451d1296cb166b037c1ab8131a2903470618
yutailang0119 <yutailang0119@gmail.com>
2024-12-22 04:47:48 +0900

    Fix Day20.part1 to use cheats(lower:)

--- Sources/AdventOfCode/Day20.swift
+++ Sources/AdventOfCode/Day20.swift
@@ -9,40 +9,8 @@ struct Day20: AdventDay {
   }
 
   func _part1(lower: Int) -> Any {
-    let marks = self.entities.map {
-      $0.compactMap(Mark.init(rawValue:))
-    }
-    let positions = marks.enumerated()
-      .flatMap { y, row in
-        row.enumerated()
-          .map { x, mark in (Position(x: x, y: y), mark) }
-      }
-    let map: [Position: Mark] = Dictionary(positions, uniquingKeysWith: { $1 })
-
-    let start = map.first { $0.value == .start }!.key
-    let end = map.first { $0.value == .end }!.key
-
-    let dijkstra = Dijkstra(
-      elements: map,
-      start: start,
-      end: end,
-      directions: [.top, .bottom, .left, .right]
-    ) { mark in
-      mark.map({ $0 != .wall }) ?? false
-    }
-    let path = dijkstra.path
-    let route = Array(path!.enumerated())
-
-    var cheats = [Int: Int]()
-    for (picosecond, position) in route {
-      let tracks = route[picosecond...].filter { $0.element.distance(to: position) <= 2 }
-      for track in tracks {
-        let saved = track.offset - picosecond - track.element.distance(to: position)
-        cheats[saved, default: 0] += 1
-      }
-    }
-
-    return cheats.filter { $0.key >= lower }
+    cheats(lower: lower)
+      .filter { $0.key >= lower }
       .reduce(0) { $0 + $1.value }
   }
 }
