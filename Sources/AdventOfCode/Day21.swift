0b3779f4349891e7f0e5bdda5f4f74c1845ef5c4
yutailang0119 <yutailang0119@gmail.com>
2024-12-23 03:52:17 +0900

    Fix Day21 to add NumericKeypad, DirectionalKeypad

--- Sources/AdventOfCode/Day21.swift
+++ Sources/AdventOfCode/Day21.swift
@@ -22,4 +22,47 @@ private extension Day21 {
       Int(String(rawValue.dropLast(1))) ?? 0
     }
   }
+
+  enum NumericKeypad: Character, CaseIterable {
+    case zero = "0"
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case a = "A"
+  }
+
+  enum DirectionalKeypad: Character, CaseIterable {
+    case up = "^"
+    case left = "<"
+    case down = "v"
+    case right = ">"
+    case a = "A"
+
+    init?(rawValue: Character) {
+      switch rawValue {
+      case "^": self = .up
+      case "<": self = .left
+      case "v": self = .down
+      case ">": self = .right
+      case "A": self = .a
+      default: return nil
+      }
+    }
+
+    init(direction: Direction) {
+      switch direction {
+      case .top: self = .up
+      case .bottom: self = .down
+      case .left: self = .left
+      case .right: self = .right
+      default: fatalError()
+      }
+    }
+  }
 }
