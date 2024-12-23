f2b2a9fd89646630b76d2b2097f92bcf143a39d9
yutailang0119 <yutailang0119@gmail.com>
2024-12-23 22:48:06 +0900

    Fix Day22 to add testSecret

--- Tests/AdventOfCodeTests/Day22.swift
+++ Tests/AdventOfCodeTests/Day22.swift
@@ -11,6 +11,17 @@ struct Day22Tests {
 
     """
 
+  @Test func testSecret() async throws {
+    do {
+      let secret = Day22.Secret(rawValue: 42)
+      #expect(secret.mix(15).rawValue == 37)
+    }
+    do {
+      let secret = Day22.Secret(rawValue: 100000000)
+      #expect(secret.prune().rawValue == 16113920)
+    }
+  }
+
   @Test func testPart1() async throws {
     let challenge = Day22(data: testData)
     try await #expect(String(describing: challenge.part1()) == "37327623")
