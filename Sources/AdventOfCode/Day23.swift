2de988dcbcf2e2b8785c787814bb34d885b2e7e0
yutailang0119 <yutailang0119@gmail.com>
2024-12-24 22:19:14 +0900

    Fix Day23 to add connections, computers

--- Sources/AdventOfCode/Day23.swift
+++ Sources/AdventOfCode/Day23.swift
@@ -12,6 +12,33 @@ private extension Day23 {
   var entities: [String] {
     data.split(separator: "\n").map(String.init)
   }
+
+  var connections: [Connection] {
+    let entities = self.entities
+
+    return entities.map { entity in
+      let splited = entity.split(separator: "-")
+      return Connection(a: String(splited[0]), b: String(splited[1]))
+    }
+  }
+
+  var computers: [String: Computer] {
+    let connections = self.connections
+
+    var computers: [String: Computer] = [:]
+    for connection in connections {
+      computers[
+        connection.a,
+        default: Computer(name: connection.a, connections: [])
+      ].connections.insert(connection.b)
+      computers[
+        connection.b,
+        default: Computer(name: connection.b, connections: [])
+      ].connections.insert(connection.a)
+    }
+
+    return computers
+  }
 }
 
 private extension Day23 {
