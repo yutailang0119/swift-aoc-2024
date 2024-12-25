bf5c3674e84338a57a1b62cf443772c242138208
yutailang0119 <yutailang0119@gmail.com>
2024-12-25 20:15:36 +0900

    Fix Day24 to add Wire, Gate

--- Sources/AdventOfCode/Day24.swift
+++ Sources/AdventOfCode/Day24.swift
@@ -15,3 +15,34 @@ private extension Day24 {
     }
   }
 }
+
+private extension Day24 {
+  struct Wire {
+    var name: String
+    var bool: Bool
+  }
+
+  struct Gate {
+    enum Operation: String {
+      case and = "AND"
+      case or = "OR"
+      case xor = "XOR"
+
+      func operate(_ a: Bool, _ b: Bool) -> Bool {
+        switch self {
+        case .and:
+          return a && b
+        case .or:
+          return a || b
+        case .xor:
+          return (a || b) && !(a && b)
+        }
+      }
+    }
+
+    var a: String
+    var b: String
+    var operation: Operation
+    var output: String
+  }
+}
