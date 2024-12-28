738a498c3d619693fd82066b36a2c08e6c01f912
yutailang0119 <yutailang0119@gmail.com>
2024-12-29 06:01:51 +0900

    Fix Day24 to add methods

--- Sources/AdventOfCode/Day24.swift
+++ Sources/AdventOfCode/Day24.swift
@@ -67,22 +67,17 @@ private extension Day24 {
     in gates: inout [String: Gate]
   ) -> Bool {
     let gate = gates[string]!
-    var cache = false
     if let caching,
-      let high = gate.dependency,
-      high < caching
+      let dependency = gate.dependency,
+       dependency < caching,
+       let cached = gate.cached
     {
-      if let cached = gate.cached {
-        return cached
-      }
-      cache = true
+      return cached
     }
     let a = value(for: gate.a, caching: caching, xs: xs, ys: ys, in: &gates)
     let b = value(for: gate.b, caching: caching, xs: xs, ys: ys, in: &gates)
     let binary = gate.operation.operate(a, b)
-    if cache {
-      gates[string]!.cached = binary
-    }
+    gates[string]!.cached = binary
     return binary
   }
 
@@ -101,6 +96,195 @@ private extension Day24 {
     case .gate(let string):
       return value(string, caching: caching, xs: xs, ys: ys, in: &gates)
     }
+  }
+
+  func swaps(
+    depth: Int,
+    corrects: inout Set<Int>,
+    outputs: inout [[String]],
+    dependencies: inout [String: Set<String>],
+    xs: inout [Bool],
+    ys: inout [Bool],
+    in gates: inout [String: Gate]
+  ) -> [String]? {
+    corrects = self.corrects(
+      xs: &xs,
+      ys: &ys,
+      in: &gates
+    )
+    guard corrects.count <= xs.count else {
+      return []
+    }
+
+    outputs = Array(repeating: [], count: xs.count)
+    dependencies = [:]
+
+    let possibles = possibles(
+      corrects: corrects,
+      outputs: &outputs,
+      dependencies: &dependencies,
+      xs: &xs,
+      ys: &ys,
+      in: &gates
+    )
+    for possible in possibles {
+      let a = gates[possible.a]!
+      let b = gates[possible.b]!
+      gates[possible.a] = b
+      gates[possible.b] = a
+      if let swaps = swaps(
+        depth: depth + 1,
+        corrects: &corrects,
+        outputs: &outputs,
+        dependencies: &dependencies,
+        xs: &xs,
+        ys: &ys,
+        in: &gates
+      ) {
+        return [possible.a, possible.b] + swaps
+      }
+      gates[possible.a] = a
+      gates[possible.b] = b
+    }
+    return nil
+  }
+
+  func corrects(
+    xs: inout [Bool],
+    ys: inout [Bool],
+    in gates: inout [String: Gate]
+  ) -> Set<Int> {
+    var bits: Set<Int> = []
+    for i in 0...xs.count {
+      if simulateBit(i, caching: false, xs: &xs, ys: &ys, in: &gates) {
+        bits.insert(i)
+      }
+    }
+    return bits
+  }
+
+  func possibles(
+    corrects: Set<Int>,
+    outputs: inout [[String]],
+    dependencies: inout [String: Set<String>],
+    xs: inout [Bool],
+    ys: inout [Bool],
+    in gates: inout [String: Gate]
+  ) -> [(a: String, b: String)] {
+    func _simulateBits(_ bit: Int) -> Bool {
+      guard simulateBit(bit, caching: true, xs: &xs, ys: &ys, in: &gates) else {
+        return false
+      }
+      for bit in corrects {
+        guard simulateBit(bit, caching: true, xs: &xs, ys: &ys, in: &gates) else {
+          return false
+        }
+      }
+      return true
+    }
+    func _gates(input: Gate.Input) -> (gates: Set<String>, index: Int) {
+      switch input {
+      case .x(let index), .y(let index):
+        return ([], index)
+      case .gate(let output):
+        return _gates(output: output)
+      }
+    }
+    func _gates(output: String) -> (gates: Set<String>, index: Int) {
+      var gate = gates[output]!
+      if let index = gate.dependency {
+        return (dependencies[output]!, index)
+      }
+      let (gateA, indexA) = _gates(input: gate.a)
+      let (gateB, indexB) = _gates(input: gate.b)
+      let dependency = gateA.union(gateB).union([output])
+      let index = max(indexA, indexB)
+      gate.dependency = index
+      gate.cached = nil
+      gates[output] = gate
+      dependencies[output] = dependency
+      outputs[index].append(output)
+      return (dependency, index)
+    }
+
+    for key in gates.keys {
+      gates[key]!.cached = nil
+      gates[key]!.dependency = nil
+    }
+    for key in gates.keys {
+      _ = _gates(output: key)
+    }
+
+    var swaps: [(a: String, b: String)] = []
+    for index in 0...xs.count where !corrects.contains(index) {
+      for a in dependencies["z\(index.bit)"]! {
+        let targetA = dependencies[a]!
+        for b in outputs[index] where !targetA.contains(b) {
+          let targetB = dependencies[b]!
+          guard !targetB.contains(a) else {
+            continue
+          }
+          let gateA = gates[a]!
+          let gateB = gates[b]!
+          gates[a] = gateB
+          gates[b] = gateA
+          if _simulateBits(index) {
+            swaps.append((a, b))
+          }
+          gates[a] = gateA
+          gates[b] = gateB
+        }
+      }
+    }
+    return swaps
+  }
+
+  func simulateBit(
+    _ bit: Int,
+    caching: Bool,
+    xs: inout [Bool],
+    ys: inout [Bool],
+    in gates: inout [String: Gate]
+  ) -> Bool {
+    let gate = "z\(bit.bit)"
+    switch bit {
+    case 0:
+      for x in [0, 1] {
+        xs[bit] = x == 1
+        for y in [0, 1] {
+          ys[bit] = y == 1
+          let zValue = value(gate, caching: nil, xs: xs, ys: ys, in: &gates)
+          guard (x + y) & 1 == zValue.int else {
+            return false
+          }
+        }
+      }
+    case xs.count:
+      for i in [0, 1] {
+        xs[bit - 1] = i == 1
+        ys[bit - 1] = i == 1
+        let zValue = value(gate, caching: caching ? bit - 1 : nil, xs: xs, ys: ys, in: &gates)
+        guard i == zValue.int else {
+          return false
+        }
+      }
+    default:
+      for i in [0, 1] {
+        xs[bit - 1] = i == 1
+        ys[bit - 1] = i == 1
+        for x in [0, 1] {
+          xs[bit] = x == 1
+          for y in [0, 1] {
+            ys[bit] = y == 1
+            let zValue = value(gate, caching: caching ? bit - 1 : nil, xs: xs, ys: ys, in: &gates)
+            guard (x + y + i) & 1 == zValue.int else {
+              return false
+            }
+          }
+        }
+      }
+    }
+    return true
   }
 }
 
